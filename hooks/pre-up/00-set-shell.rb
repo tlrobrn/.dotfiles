9bfa7194810cad2017fd9c0305aad0bcb2368da2
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2022-02-25 16:57:21 -0700

    install zsh if necessary

--- hooks/pre-up/00-set-shell.rb
+++ hooks/pre-up/00-set-shell.rb
@@ -4,11 +4,23 @@ require_relative "../os_command"
 
 class SetShell < OSCommand
   def mac
+    `brew install zsh` if install_zsh?
+
     `chsh -s $(which zsh)`
   end
 
   def debian
+    `sudo apt install zsh` if install_zsh?
+
     `sudo chsh $(whoami) -s $(which zsh)`
+  end
+
+  def skip?
+    `echo $0`.include?("zsh")
+  end
+
+  def install_zsh?
+    `which zsh`.empty?
   end
 end
 
