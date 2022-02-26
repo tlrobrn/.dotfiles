56e55277a9405d1040f46a00b01105b3a90b3533
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2022-02-25 17:30:39 -0700

    try installing term everywhere

--- hooks/post-up/00-install-italic-term.rb
+++ hooks/post-up/00-install-italic-term.rb
@@ -4,6 +4,14 @@ require_relative "../os_command"
 
 class InstallItalicTerm < OSCommand
   def mac
+    install
+  end
+
+  def debian
+    install
+  end
+
+  def install
     stream_command("tic -o #{File.expand_path("~/.terminfo")} #{File.expand_path("~/.config/term/xterm-256color.terminfo")}")
   end
 end
