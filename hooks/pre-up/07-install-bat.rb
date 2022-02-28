679cbda224f57203954d93c317c3a86860b79156
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2022-02-28 14:49:33 +0000

    don't install bat on linux

--- hooks/pre-up/07-install-bat.rb
+++ hooks/pre-up/07-install-bat.rb
@@ -7,11 +7,6 @@ class InstallBat < OSCommand
     stream_command("brew install bat")
   end
 
-  def debian
-    stream_command("sudo apt-get -y install bat")
-    stream_command("ln -s $(which batcat) #{File.expand_path("~/.local/bin/bat")}")
-  end
-
   def skip?
     !`which bat`.empty?
   end
