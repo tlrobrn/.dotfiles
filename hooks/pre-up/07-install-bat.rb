223f6ef1a5c2fff746b6253b243cd70e7610aacd
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2022-03-08 15:48:04 +0000

    install bat on pco cloud box

--- hooks/pre-up/07-install-bat.rb
+++ hooks/pre-up/07-install-bat.rb
@@ -7,8 +7,22 @@ class InstallBat < OSCommand
     stream_command("brew install bat")
   end
 
+  def debian
+    stream_command('sudo apt-get install -y -o Dpkg::Options::="--force-overwrite" bat')
+    stream_command("mkdir -p #{local_bin_path}")
+    stream_command("ln -s $(which batcat) #{link_path}")
+  end
+
   def skip?
     !`which bat`.empty?
+  end
+
+  def local_bin_path
+    File.dirname(link_path)
+  end
+
+  def link_path
+    File.expand_path("~/.local/bin/bat")
   end
 end
 
