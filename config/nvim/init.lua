b1e191df23700ed715969f005d3880486a843eb0
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2022-11-16 09:22:22 -0700

    mostly working init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,59 +1,71 @@
 -- Manage Plugins
-require"paq" {
-  "aktersnurra/no-clown-fiesta.nvim"; -- colorscheme
-  "neovim/nvim-lspconfig"; -- Configurations for nvim LSP
-  "jose-elias-alvarez/null-ls.nvim"; -- LSP shim for other tools
-  "nvim-lualine/lualine.nvim"; -- Status line
-  "savq/paq-nvim"; -- Package Manager
-  "terrortylor/nvim-comment"; -- comment toggler
-  {"glepnir/lspsaga.nvim", branch="main"}; -- LSP UI
-  {"ms-jpq/chadtree", branch="chad", run="python3 -m chadtree deps"}; -- file tree
-  {"ms-jpq/coq.artifacts", branch="artifacts"}; -- Completion snippets
-  {"ms-jpq/coq.thirdparty", branch="3p"}; -- Completion 3rdparty libs
-  {"ms-jpq/coq_nvim", branch="coq"}; -- Completion
-  {"nvim-tree/nvim-web-devicons", opt=true}; -- font icons
-  "nvim-lua/plenary.nvim"; --fuzzy finder dep
-  {"nvim-telescope/telescope.nvim", branch="0.1.x"}; -- fuzzy finder
-  {"nvim-treesitter/nvim-treesitter", run=function() vim.cmd "TSUpdate" end}; -- treesitter
-}
+require("paq")({
+	"aktersnurra/no-clown-fiesta.nvim", -- colorscheme
+	"neovim/nvim-lspconfig", -- Configurations for nvim LSP
+	"jose-elias-alvarez/null-ls.nvim", -- LSP shim for other tools
+	"nvim-lualine/lualine.nvim", -- Status line
+	"savq/paq-nvim", -- Package Manager
+	"terrortylor/nvim-comment", -- comment toggler
+	{ "glepnir/lspsaga.nvim", branch = "main" }, -- LSP UI
+	{ "ms-jpq/chadtree", branch = "chad", run = "python3 -m chadtree deps" }, -- file tree
+	{ "ms-jpq/coq.artifacts", branch = "artifacts" }, -- Completion snippets
+	{ "ms-jpq/coq.thirdparty", branch = "3p" }, -- Completion 3rdparty libs
+	{ "ms-jpq/coq_nvim", branch = "coq" }, -- Completion
+	{ "nvim-tree/nvim-web-devicons", opt = true }, -- font icons
+	"nvim-lua/plenary.nvim", --fuzzy finder dep
+	{ "nvim-telescope/telescope.nvim", branch = "0.1.x" }, -- fuzzy finder
+	{
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			vim.cmd("TSUpdate")
+		end,
+	}, -- treesitter
+})
 
 local keymap = vim.keymap.set
 
 -- colorscheme
-require"no-clown-fiesta".setup{
-  styles = {
-    comments = { italic = true },
-  },
-}
-vim.cmd[[colorscheme no-clown-fiesta]]
+require("no-clown-fiesta").setup({
+	styles = {
+		comments = { italic = true },
+	},
+})
+vim.cmd([[colorscheme no-clown-fiesta]])
 
 -- tabs
 vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smarttab = true
 vim.opt.autoindent = true
 
 -- line numbers
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
 -- Case insensitive search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.hlsearch = true
-vim.o.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
--- Nowrap
-vim.wo.wrap = false
+-- Base editor configs
+vim.opt.wrap = false
+vim.opt.showmatch = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.g.mapleader = " "
+-- vim.cmd[[highlight Comment cterm=italic]]
+-- vim.cmd[[highlight htmlArg cterm=italic]]
+-- vim.cmd[[highlight Type cterm=italic]]
 
 -- JK for esc
-keymap("i", "jk", "<ESC>", { noremap=true, silent=true })
+keymap("i", "jk", "<ESC>", { noremap = true, silent = true })
 
 -- Comment toggling
-require('nvim_comment').setup({comment_empty = false})
+require("nvim_comment").setup({ comment_empty = false })
 
 -- fuzzy finder
 local builtin = require("telescope.builtin")
@@ -64,88 +76,127 @@ keymap("n", "<leader>be", builtin.buffers, {})
 keymap("n", "<leader>fh", builtin.help_tags, {})
 
 -- treesitter
-require"nvim-treesitter.configs".setup{
-  highlight = {
-    enable = true,
-    disable = { "lua" },
-  },
-  auto_install = false,
-}
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
+		disable = { "lua" },
+	},
+	auto_install = false,
+})
 
 -- Statusline
-require"lualine".setup{}
+require("lualine").setup({})
 
 -- Completion by COQ
 vim.g.coq_settings = { auto_start = "shut-up" }
-local coq = require"coq"
-require"coq_3p" {
-  { src = "nvimlua", short_name = "nLUA", conf_only = true },
-}
+local coq = require("coq")
+require("coq_3p")({
+	{ src = "nvimlua", short_name = "nLUA", conf_only = true },
+})
 
 -- Filetree browser
 keymap("n", "<leader>nt", "<cmd>CHADopen<cr>", { silent = true })
 vim.g.chadtree_settings = {
-  ["theme.text_colour_set"] = "nerdtree_syntax_dark",
+	["theme.text_colour_set"] = "nerdtree_syntax_dark",
 }
 
 -- LSP
-local lspconfig = require"lspconfig"
+local lspconfig = require("lspconfig")
 
 -- Setup for nvim lua
-lspconfig.sumneko_lua.setup(coq.lsp_ensure_capabilities {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you"re using (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {"vim"},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-	checkThirdParty = false,
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-})
+lspconfig.sumneko_lua.setup(coq.lsp_ensure_capabilities({
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you"re using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+}))
 
 -- Setup for stree
-lspconfig.syntax_tree.setup{
-  cmd = {"bundle", "exec", "stree", "lsp" },
-}
+lspconfig.syntax_tree.setup({
+	cmd = { "bundle", "exec", "stree", "lsp" },
+})
 
 -- Setup for ts/js
-lspconfig.tsserver.setup(coq.lsp_ensure_capabilities{})
+lspconfig.tsserver.setup(coq.lsp_ensure_capabilities({}))
 
 -- autoformat
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.{js,jsx,ts,tsx}",
-  callback = function()
-    -- vim.lsp.buf.formatting_seq_sync()
-    vim.lsp.buf.format()
-  end,
-  group = vim.api.nvim_create_augroup("AutocmdForTSJSFormatting", {}),
+	pattern = "*.{js,jsx,ts,tsx}",
+	callback = function()
+		-- vim.lsp.buf.formatting_seq_sync()
+		vim.lsp.buf.format()
+	end,
+	group = vim.api.nvim_create_augroup("AutocmdForTSJSFormatting", {}),
 })
 
-require"lspsaga".init_lsp_saga()
+require("lspsaga").init_lsp_saga()
 
--- Rubocop setup
-local null_ls = require"null-ls"
-null_ls.setup{
-  sources = {
-    null_ls.builtins.diagnostics.rubocop.with{
-      command = "bundle",
-      args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.diagnostics.rubocop._opts.args),
-    },
-  },
-}
+-- Formatting setup
+local null_ls = require("null-ls")
+
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local on_attach = function(client, bufnr)
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
+end
+
+null_ls.setup({
+	on_attach = on_attach,
+	sources = {
+		null_ls.builtins.diagnostics.rubocop.with({
+			command = "bundle",
+			args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.diagnostics.rubocop._opts.args),
+		}),
+		null_ls.builtins.diagnostics.eslint.with({
+			prefer_local = "node_modules/.bin",
+			timeout = 10000,
+		}),
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.eslint.with({
+			prefer_local = "node_modules/.bin",
+			timeout = 10000,
+		}),
+		null_ls.builtins.formatting.rubocop.with({
+			command = "bundle",
+			args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.formatting.rubocop._opts.args),
+		}),
+	},
+})
 
 -- Lsp finder find the symbol definition implement reference
 -- if there is no implement it will hide
@@ -154,7 +205,7 @@ null_ls.setup{
 keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
 
 -- Code action
-keymap({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
 
 -- Rename
 keymap("n", "gr", "<cmd>Lspsaga rename<CR>", { silent = true })
@@ -177,14 +228,14 @@ keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
 
 -- Only jump to error
 keymap("n", "[E", function()
-  require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+	require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
 end, { silent = true })
 keymap("n", "]E", function()
-  require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+	require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
 end, { silent = true })
 
 -- Outline
-keymap("n","<leader>o", "<cmd>LSoutlineToggle<CR>",{ silent = true })
+keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", { silent = true })
 
 -- Hover Doc
 keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
