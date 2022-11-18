6463bb57c671bf68e62426f4f54d5de7ebc10b72
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2022-11-18 17:37:41 +0000

    install stylua for lua formatting

---
+++ hooks/pre-up/17-install-stylua.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+
+require_relative "../os_command"
+
+class InstallStylua < OSCommand
+  def mac
+    stream_command("curl -L https://github.com/JohnnyMorganz/StyLua/releases/download/#{version}/stylua-macos-aarch64.zip -o #{zip_path}")
+    stream_command("unzip #{zip_path} -d /usr/local/bin")
+    stream_command("rm #{zip_path}")
+  end
+
+  def debian
+    stream_command("curl -L https://github.com/JohnnyMorganz/StyLua/releases/download/#{version}/stylua-linux.zip -o #{zip_path}")
+    stream_command("unzip #{zip_path} -d #{File.expand_path("~/.local/bin")}")
+    stream_command("rm #{zip_path}")
+  end
+
+  def skip?
+    !`which stylua`.empty?
+  end
+
+  def zip_path
+    "/tmp/stylua.zip"
+  end
+
+  def version
+    "v0.15.2"
+  end
+end
+
+InstallStylua.run!
