2680afb0c72440f37a8607c2f7bd464f72e07497
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2022-12-14 09:14:51 -0700

    let null-ls handle ruby formatting

--- config/nvim/after/ftplugin/ruby.lua
+++ config/nvim/after/ftplugin/ruby.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function()
-    vim.lsp.buf.format()
-  end,
-  group = vim.api.nvim_create_augroup("AutocmdForRubyFormatting", {}),
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   callback = function()
+--     vim.lsp.buf.format()
+--   end,
+--   group = vim.api.nvim_create_augroup("AutocmdForRubyFormatting", {}),
+-- })
