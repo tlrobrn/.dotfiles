4b726742ace050a10d6a89e4bd677210fb2353fb
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2023-02-13 08:15:11 -0700

    remove files even possibly related to nvim setup

--- config/nvim/after/ftplugin/ruby.lua
+++
@@ -1,6 +0,0 @@
--- vim.api.nvim_create_autocmd("BufWritePre", {
---   callback = function()
---     vim.lsp.buf.format()
---   end,
---   group = vim.api.nvim_create_augroup("AutocmdForRubyFormatting", {}),
--- })
