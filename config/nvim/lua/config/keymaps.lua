35d8afa78e331613c9942e0c14b181d554bc8443
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2023-02-13 21:26:15 +0000

    move lines around

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,11 @@ keymap("i", "jk", "<ESC>", { noremap = true, silent = true })
 
 -- paste over selected text without yanking it
 keymap("v", "p", '"_dP')
+
+-- move lines
+keymap("n", "<C-j>", ":m .+1<CR>==")
+keymap("v", "<C-j>", ":m '>+1<CR>gv=gv")
+keymap("i", "<C-j>", "<Esc>:m .+1<CR>==gi")
+keymap("n", "<C-k>", ":m .-2<CR>==")
+keymap("v", "<C-k>", ":m '<-2<CR>gv=gv")
+keymap("i", "<C-k>", "<Esc>:m .-2<CR>==gi")
