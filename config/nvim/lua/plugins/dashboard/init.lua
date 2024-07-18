3ae3d11663c586a1fb5662480e012471e4914c87
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2024-07-18 14:57:57 -0600

    use icons that work

--- config/nvim/lua/plugins/dashboard/init.lua
+++ config/nvim/lua/plugins/dashboard/init.lua
@@ -13,7 +13,7 @@ return {
     local dashboard = require("alpha.themes.dashboard")
     dashboard.section.header.val = header
     dashboard.section.buttons.val = {
-      dashboard.button("f", "  Find file", "<cmd>Telescope find_files<cr>"),
+      dashboard.button("f", "󰈔  Find file", "<cmd>Telescope find_files<cr>"),
       dashboard.button("g", "  Grep", "<cmd>Telescope live_grep<cr>"),
       dashboard.button("e", "  New file", "<cmd>ene<bar>startinsert<cr>"),
       dashboard.button("q", "  Quit", "<cmd>q<cr>"),
